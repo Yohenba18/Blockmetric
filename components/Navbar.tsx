207930725abc591591b2dc0ab0780ff68f9cf5ba
Yohenba18 <roddik12kshetri@gmail.com>
2021-09-23 16:55:45 +0530

    Fotter improvement basic Navbar

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -2,21 +2,21 @@ import Link from "next/link";
 
 const Navbar: React.FC = () => {
   return (
-    <div className="flex">
+    <div className="flex justify-between max-w-7xl mx-auto">
       <div>
         <h1>Logo</h1>
       </div>
       <div className="flex">
-        <Link href="/">Analysis</Link>
-        <Link href="/">Analysis</Link>
-        <Link href="/">Analysis</Link>
+        <div><Link href="/">Analysis</Link></div>
+        <div><Link href="/">Analysis</Link></div>
+        <div><Link href="/">Analysis</Link></div>
       </div>
-      <div className="flex">
+      <div className="flex gap-2">
         <div>LOGIN</div>
-        <hr />
+        <div>|</div>
         <div>SIGN IN</div>
       </div>
-      <div className="flex">
+      <div className="flex md:hidden">
         <h1>Hamburger Icon</h1>
       </div>
     </div>
